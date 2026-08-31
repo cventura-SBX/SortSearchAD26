0e99ab28eaa33eb993df12dc5af580f145a0f2a4
cventura <cventura@outlook.com>
2026-08-31 11:37:39 -0600

    busqueda con datos ya ordenados

---
+++ SortSearchAD26/Search.h
@@ -0,0 +1,8 @@
+#pragma once
+class Search
+{
+public:
+	static int busquedaBin(int[], int, int);
+
+};
+
--- SortSearchAD26/SortSearchAD26.cpp
+++ SortSearchAD26/SortSearchAD26.cpp
@@ -1,20 +1,13 @@
-// SortSearchAD26.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Search.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //ya esta ordenado el arregloo
+    int array[10]{ 1,2,5,6,7,9,10,17,99,100};
+    
+    int index = Search::busquedaBin(array, sizeof(array) / sizeof(array[0]), 17);
+
+    std::cout << "index at: " << index << " valor: " << array[index];
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
